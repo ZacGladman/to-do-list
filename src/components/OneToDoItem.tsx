783e61f24017de0161a14a1882913eb1f9cc2ed0
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-12-02 13:49:28 +0000

    Create basic OneToDoItem with all components

--- src/components/OneToDoItem.tsx
+++ src/components/OneToDoItem.tsx
@@ -1,7 +1,17 @@
-export default function OneToDoItem(): JSX.Element {
+import { IOneToDoItem } from "../utils/IOneToDoItem";
+
+interface OneToDoItemProps{
+    item: IOneToDoItem
+}
+
+export default function OneToDoItem(props: OneToDoItemProps): JSX.Element {
+    const item = props.item
     return (
         <>
-            
+          <h2>{item.title}</h2>
+          <h3>Status: {item.status} | Importance: {item.importance} | Due: {item.dueDate}</h3>
+          <p>{item.description}</p>
+
         </>
     )
 }
